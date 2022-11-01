e82d843f0f5f0f47ddf894bf50ce550c3761e519
Eduardocrugo <106431681+Eduardocrugo@users.noreply.github.com>
2022-11-01 19:44:26 +0000

    Delete P05 directory

--- P05/P05/boolean_operators.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-    int a = 10;
-    int b = 7;
-    std::cout << ( a > 4 && b > 7 ) << '\n'; 
-    std::cout << ( a > 4 || b > 7 ) << '\n';
-    std::cout << (!( a > 4 && b > 7 )) << '\n';
-    return 0;
-}
--- P05/P05/desinflado.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-    char letraA = 'a';
-    char letraM = 'm';
-    letraA = letraA - 32;
-    letraM = letraM - 32;
-    std::cout << letraA << '\n' << letraM;
-}
